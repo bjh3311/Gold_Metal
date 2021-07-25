7270cf9759e8869b4b21e24e69643efb732fe403
bjh3311 <bjh3311@naver.com>
2021-07-25 21:50:11 +0900

    warn상태 추가

--- Assets/Script/Boss_1.cs
+++ Assets/Script/Boss_1.cs
@@ -10,15 +10,21 @@ public class Boss_1 : MonoBehaviour
     private Animator animator;
     private SpriteRenderer rend;
     private Transform transform;
-    public enum CurrentState { idle, walk };
+    private Transform playerTransform;
+    public enum CurrentState { idle, walk ,warn };
     public CurrentState curState = CurrentState.idle;
+
+    private int walkOridle = 1;//1이면 idle -1이면 walk
+
+    private float maxtime = 3.0f;
+    private float delaytime;
     // Start is called before the first frame update
     void Start()
     {
         transform = this.gameObject.GetComponent<Transform>();
         rend = this.gameObject.GetComponent<SpriteRenderer>();
         animator = this.gameObject.GetComponent<Animator>();
-
+        playerTransform =GameObject.FindWithTag("Player").GetComponent<Transform>();
         StartCoroutine(CheckState());
         StartCoroutine(CheckStateForAction());
     }
@@ -26,19 +32,22 @@ public class Boss_1 : MonoBehaviour
     {
         while(true)
         {
-            yield return new WaitForSeconds(0.2f);
-            float dist = Vector2.Distance(stat.playerTransform.position, transform.position);
-            if (dist <= stat.view)//거리가 view안에 있다면 curState는 walk
+            MoveTime();
+            float dist = Vector2.Distance(playerTransform.position, transform.position);
+            if (dist <= stat.view&&Input.GetButtonDown("Fire1"))//거리가 view안에 있고 마우스 좌클릭하면 curState는 walk
             {
-                curState = CurrentState.walk;
+                curState = CurrentState.warn;
             }
-            else
+            else if(walkOridle==1)
             {
                 curState = CurrentState.idle;
             }
+            else if(walkOridle==-1)
+            {
+                curState = CurrentState.walk;
+            }
+            yield return null;
         }
-        
-
     }
     IEnumerator CheckStateForAction()
     {
@@ -48,9 +57,14 @@ public class Boss_1 : MonoBehaviour
             {
                 case CurrentState.idle:
                     animator.SetBool("isMoving", false);
+                    if(delaytime>maxtime)
+                    {
+                        delaytime = 0;
+                        walkOridle = walkOridle * -1;
+                    }
                     break;
                 case CurrentState.walk:
-                    if (stat.playerTransform.position.x - transform.position.x < 0)//타겟이 왼쪽에 있다면
+                    if (playerTransform.position.x - transform.position.x < 0)//타겟이 왼쪽에 있다면
                     {
                         rend.flipX = false;
                     }
@@ -60,15 +74,28 @@ public class Boss_1 : MonoBehaviour
                     }
                     MoveToTarget();
                     animator.SetBool("isMoving", true);
+                    if(delaytime>maxtime)
+                    {
+                        delaytime = 0;
+                        walkOridle = walkOridle * -1;
+                    }
+                    break;
+                case CurrentState.warn:
+                    Debug.Log("경고상태_1!!");
+                    yield return new WaitForSeconds(1.0f);
                     break;
 
             }
             yield return null;
         }
     }
+    public void MoveTime()
+    {
+        delaytime +=Time.deltaTime;
+    }
     public void MoveToTarget()
     {
-        float dir = stat.playerTransform.position.x - transform.position.x;
+        float dir = playerTransform.position.x - transform.position.x;
         dir = (dir < 0) ? -1 : 1;
         transform.Translate(new Vector2(dir, 0) * stat.moveSpeed * Time.deltaTime);
     }
---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = player.GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
