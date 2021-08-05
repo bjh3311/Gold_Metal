b4c9112e45bb42b345966c9f70869c92f0cc9ac1
bjh3311 <bjh3311@naver.com>
2021-08-05 16:51:11 +0900

    간단한 대화 UI 구현

--- Assets/Script/TalkManager.cs
+++ Assets/Script/TalkManager.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 
 public class TalkManager : MonoBehaviour
 {
+    public GameObject talkPanel;
     public Text talkText;
     public GameObject scanObject;
+    public bool isAction;
     public void Action(GameObject scanObj)
     {
-        scanObject = scanObj;
-        talkText.text = "이것의 이름은 "+scanObject.name+"이라고 한다.";
+        if(isAction)//Exit Action
+        {
+            isAction = false;
+        }
+        else
+        {
+            isAction = true;
+            scanObject = scanObj;
+            talkText.text = "이것의 이름은 " + scanObject.name + "이라고 한다.";
+        }
+        talkPanel.SetActive(isAction);
     }
 }
