0b87bc074a1e3bc4fee0b51f5db0b16245121009
bjh3311 <bjh3311@naver.com>
2021-08-22 15:03:14 +0900

    json id 따라초상화색깔 및 투명도 변경

--- Assets/Script/TalkManager.cs
+++ Assets/Script/TalkManager.cs
@@ -56,14 +56,14 @@ public class TalkManager : MonoBehaviour
         {
             if(pData.characterData[index].id=="0")//id가 0이라면 즉, 플레이어라면,C#에서는 부등호 비교가 빠르다.
             {
-                playerPortrait.color = new Color(255, 255, 255);//플레이어는 밝게
-                npcPortrait.color = new Color(56, 56, 56);//npc는 어둡게
+                playerPortrait.color = new Color32(255, 255, 255,255);//플레이어는 밝게
+                npcPortrait.color = new Color32(56, 56, 56,255);//npc는 어둡게
             }
-            else//id가 0이외의 즉 NPC라면
+            else//id가 0이 아니라면 즉 NPC라면
             {
-                playerPortrait.color = new Color(56, 56, 56);//플레이어는 어둡게
-                npcPortrait.color = new Color(255, 255, 255);//npc는 밝게
-            }
+                playerPortrait.color = new Color32(56, 56, 56,255);//플레이어는 어둡게
+                npcPortrait.color = new Color32(255, 255, 255,255);//npc는 밝게
+            }//Color32의 마지막 인자는 투명도를 나타냄. 0이면 완전투명이어서 안보이고 255면 완전 불투명
             talkText.text = pData.characterData[index].language[lanNum].text;
         }
         else
@@ -71,6 +71,5 @@ public class TalkManager : MonoBehaviour
             talkPanel.SetActive(false);
             return;
         }
-        
     }
 }
