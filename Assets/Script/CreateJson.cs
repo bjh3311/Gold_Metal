0662f4b05e44226b44d83c1a220256841ed58d8d
bjh3311 <bjh3311@naver.com>
2021-08-29 23:54:58 +0900

    json 배열로 저장 구현중

--- Assets/Script/CreateJson.cs
+++ Assets/Script/CreateJson.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 [System.Serializable]
 class Contents//언어별 대화 내용
 {
@@ -24,16 +25,6 @@ public class CreateJson : MonoBehaviour
     {   
         Data info=new Data();
         info.characterData=new CharacterData[2];
-        info.characterData[0].id="배진환";
-        info.characterData[0].language=new language[3];
-        info.characterData[0].language[0]="한국어";
-        info.characterData[0].langauge[1]="영어";
-        info.characterData[0].langauge[2]="일본어";
-        info.characterData[1].id="장한준";
-        info.characterData[1].language=new langague[3];
-        info.characterData[1].language[0]="한국어";
-        info.characterData[1].langauge[1]="영어";
-        info.characterData[1].langauge[2]="일본어";
         string save=JsonUtility.ToJson(info);
         if(save!=null)
         {
