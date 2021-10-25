7690be823ce1b10c6d4db35f19f5c031ddc58fa1
bjh3311 <bjh3311@naver.com>
2021-10-26 01:25:21 +0900

    점프

--- Assets/Script/Ninja.cs
+++ Assets/Script/Ninja.cs
@@ -5,9 +5,17 @@ using UnityEngine.UI;
 
 public class Ninja : MonoBehaviour
 {
+    Rigidbody2D rigid;
+    Animator anim;
+    private void Start() 
+    {
+        rigid=this.gameObject.GetComponent<Rigidbody2D>();
+        anim=this.gameObject.GetComponent<Animator>();
+    }
     public void Jump()
     {
-        Debug.Log("점프버튼");
+        rigid.AddForce(new Vector2(0,3.0f),ForceMode2D.Impulse);//점프
+        Debug.Log("점프 버튼 눌름");
     }
     public void Attack()
     {
