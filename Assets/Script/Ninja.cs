aa37639629f8dcfed8a1477aceea38c36a4bb717
bjh3311 <bjh3311@naver.com>
2021-10-25 16:30:08 +0900

    모바일 터치 확인

---
+++ Assets/Script/Ninja.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ninja : MonoBehaviour
+{
+    public void Jump()
+    {
+        Debug.Log("점프버튼");
+    }
+    public void Attack()
+    {
+        Debug.Log("Attack");
+    }
+    public void Crouch()
+    {
+        Debug.Log("Crouch");
+    }
+}
