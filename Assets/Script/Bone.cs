61ddd405ccc53b7ea0f27973683728dc854ff489
bjh3311 <bjh3311@naver.com>
2021-12-10 16:51:51 +0900

    뼈 애니메이션 완성

--- Assets/Script/Bone.cs
+++ Assets/Script/Bone.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Bone : MonoBehaviour
 {
     BoxCollider2D box;
+    Animator anim;
     private void Awake() 
     {
         box=this.gameObject.GetComponent<BoxCollider2D>();
+        anim=this.gameObject.GetComponent<Animator>();
     }
    private void OnTriggerEnter2D(Collider2D col)
    {
        if(col.gameObject.CompareTag("Weapon"))
        {
            box.enabled=false;//박스를 끈다
+           anim.SetBool("isBreak",true);
        }
+   }
+   public void Off()
+   {
+       this.gameObject.SetActive(false);//부서지면 끈다
    }
 }
