dd2f032740a65ed411c118a2c2b4286a0720501c
bjh3311 <bjh3311@naver.com>
2021-12-11 20:12:55 +0900

    얼마나 왔는지 나타내는 UI추가

---
+++ Assets/Script/UI/HowLong.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HowLong : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image nowWhere;
+    private float dis=0;
+    void Start()
+    {
+        nowWhere=this.gameObject.GetComponent<Image>();
+        StartCoroutine("plus");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowWhere.fillAmount=dis/100f;
+    }
+    IEnumerator plus()
+    {
+        while(true)
+        {
+            if(Time.timeScale!=0)
+            {
+                dis=dis+0.1f;
+            }
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+    }
+}
