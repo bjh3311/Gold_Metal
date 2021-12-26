6d5dfe1a0eff2365729a0ed6e5fd8dc9ebc55ad2
bjh3311 <bjh3311@naver.com>
2021-12-26 20:22:43 +0900

    엔딩 효과

---
+++ Assets/Script/UI/Ending.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine; 
+public class Ending : MonoBehaviour 
+{
+    private SpriteRenderer Sr; 
+    private float threshold;
+    [SerializeField]
+    private float dissolveTime;
+    private void Start() 
+    { 
+        Sr=this.gameObject.GetComponent<SpriteRenderer>();
+        threshold = Sr.material.GetFloat("_Threshold");
+    }
+    public void UpdateDissolve()
+    {
+        StartCoroutine("ActiveDissolve");    
+    } 
+    IEnumerator ActiveDissolve()
+    {
+        while(true)
+        {
+            threshold+=0.1f;
+            yield return new WaitForSeconds(dissolveTime);
+            Sr.material.SetFloat("_Threshold", threshold);
+            if(threshold>1.01)
+            {
+                break;
+            }
+        }
+        
+    }
+}
