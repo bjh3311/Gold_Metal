00b56c82a34f03779b7dd01aacaede74fcc94320
bjh3311 <bjh3311@naver.com>
2022-01-06 17:47:43 +0900

    로그인 틀은 제작 완료

---
+++ Assets/Script/Main/Login.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Main;
+    public GameObject Sign;
+
+    public GameObject Password;
+    public void GoToSignUp()
+    {
+        Main.SetActive(false);
+        Sign.SetActive(true);
+    }
+    public void Back()
+    {
+        Main.SetActive(true);
+        Sign.SetActive(false);
+        Password.SetActive(false);
+    }
+    public void SignUp()
+    {
+
+    }
+    public void LogIn()
+    {       
+
+    }
+    public void PasswordScene()
+    {
+        Main.SetActive(false);
+        Password.SetActive(true);
+    }
+    public void Submit()
+    {
+
+    }
+}
