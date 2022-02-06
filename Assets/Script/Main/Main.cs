a6108ff0ec61531d077e4c8ae06842b36e4ba029
bjh3311 <bjh3311@naver.com>
2022-02-06 17:39:55 +0900

    스테이지별 버튼 활성화

--- Assets/Script/Main/Main.cs
+++ Assets/Script/Main/Main.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.IO;
 using TMPro;
+
 
 public class Main : MonoBehaviour
 {
@@ -14,6 +18,7 @@ public class Main : MonoBehaviour
 
     public Button[] StageButton;
     public Image[] StageImage;
+    private Button[] Active_StageButton;
 
     public Text[] Texts;
 
@@ -23,7 +28,7 @@ public class Main : MonoBehaviour
     
     public Image Bar;
 
-    private int Stage;//몇 스테이지까지 클리어했는지 뜻한다. Awake에서 json정보로 받아온다
+    private static int Stage;//몇 스테이지까지 클리어했는지 뜻한다. Awake에서 json정보로 받아온다
     public void Play()//Play 버튼
     {
         for(int i=0;i<StageImage.Length;i++)
@@ -33,7 +38,7 @@ public class Main : MonoBehaviour
         ButtonDisabled(MainButton);
         StartCoroutine("ButtonTrans",MainImage);
         StartCoroutine("ButtonUnTrans",StageImage); 
-        StartCoroutine("ButtonEnabled",StageButton);
+        StartCoroutine("ButtonEnabled",Active_StageButton);
         StartCoroutine("TextUnTrans",Texts);
     }
     private void Awake() 
@@ -41,11 +46,14 @@ public class Main : MonoBehaviour
         Time.timeScale=1;//게임을 플레이하다가 정지하고 나오면 Time.timescale이 0으로 설정되기 때문에
         //Main이 Scene이 불러질 때 마다 Time.timescale을 1로 해준다
         Stage=LoadJsonData_FromAsset();
-        for(int i=1;i<=Stage;i++)
+        for(int i=1;i<=Stage;i++)//클리어한 Stage까지만 활성화
         {
             StageImage[i].color=new Color32(255,255,255,0);
             StageImage[i+4].color=new Color32(255,255,255,0);
         }
+        Active_StageButton=new Button[Stage+1];
+        Array.Copy(StageButton,0,Active_StageButton,0,Stage+1);
+        
         
     }
     public void SelectToMainButton()//Stage 선택창에서 Main으로 돌아가는 버튼
@@ -213,6 +221,6 @@ public class Main : MonoBehaviour
         string pAsset;
         pAsset=File.ReadAllText(Application.dataPath+"/Json"+"/User.json");
         User temp=JsonUtility.FromJson<User>(pAsset);
-        return int.Parse(temp.Stage);//catch문이 실행이안되면, 즉 파일이 존재하면 실행된다.
+        return int.Parse(temp.Stage);
     }
 }
