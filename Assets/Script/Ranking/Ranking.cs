461cad333017c9370c5cf5595d67f8add1bbc4ce
bjh3311 <bjh3311@naver.com>
2022-02-26 00:19:16 +0900

    랭킹시스템 php

---
+++ Assets/Script/Ranking/Ranking.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+using UnityEngine.Networking;
+
+public class Ranking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string RankingUrl;
+    private string num;//몇번 스테이지를 골랐는지
+    private void Awake()
+    {
+        RankingUrl="bjh3311.cafe24.com/Ranking.php";
+    }
+
+    // Update is called once per frame
+    public void Back()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void LoadRank(string stage)
+    {
+        if(stage=="All")
+        {
+            num="0";// 모든 Stage면 0
+        }
+        else if(stage=="Stage1")
+        {
+            num="1";
+        }
+        else if(stage=="Stage2")
+        {
+            num="2";
+        }
+        else if(stage=="Stage3")
+        {
+            num="3";
+        }
+        else if(stage=="Stage4")
+        {
+            num="4";
+        }
+        StartCoroutine("Load");
+    }
+    private IEnumerator Load()//DB에서  Stage 랭킹 불러오기
+    {
+        WWWForm form=new WWWForm();
+        form.AddField("Stage",num);
+        UnityWebRequest webRequest=UnityWebRequest.Post(RankingUrl,form);
+        yield return webRequest.SendWebRequest();
+        string temp=webRequest.downloadHandler.text;
+    }
+}
