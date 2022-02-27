7f487e8fe0515f013f6c92ee5fcd3bf98c803ebb
bjh3311 <bjh3311@naver.com>
2022-02-27 15:44:31 +0900

    랭킹 생성

--- Assets/Script/Ranking/Ranking.cs
+++ Assets/Script/Ranking/Ranking.cs
@@ -12,13 +12,17 @@ public class Ranking : MonoBehaviour
     // Start is called before the first frame update
     private string RankingUrl;
     private string num;//몇번 스테이지를 골랐는지
-    public GameObject sentence;//몇등,ID,죽은횟수
-    public GameObject Content;
+    public GameObject sentence;
+    public GameObject[] Content;
+    public GameObject[] Scroll;
     private void Awake()
     {
         RankingUrl="bjh3311.cafe24.com/Ranking.php";
+        for(int i=0;i<=4;i++)
+        {
+            StartCoroutine("Load",i);
+        }
     }
-
     // Update is called once per frame
     public void Back()
     {
@@ -26,29 +30,32 @@ public class Ranking : MonoBehaviour
     }
     public void LoadRank(string stage)
     {
+        foreach(GameObject temp in Scroll)
+        {
+            temp.SetActive(false);
+        }
         if(stage=="All")
         {
-            num="0";// 모든 Stage면 0
+            Scroll[0].SetActive(true);
         }
         else if(stage=="Stage1")
         {
-            num="1";
+            Scroll[1].SetActive(true);
         }
         else if(stage=="Stage2")
         {
-            num="2";
+            Scroll[2].SetActive(true);
         }
         else if(stage=="Stage3")
         {
-            num="3";
+            Scroll[3].SetActive(true);
         }
         else if(stage=="Stage4")
         {
-            num="4";
+            Scroll[4].SetActive(true);
         }
-        StartCoroutine("Load");
     }
-    private IEnumerator Load()//DB에서  Stage 랭킹 불러오기
+    private IEnumerator Load(int num)//DB에서  Stage 랭킹 불러오기
     {
         WWWForm form=new WWWForm();
         form.AddField("Stage",num);
@@ -56,17 +63,15 @@ public class Ranking : MonoBehaviour
         yield return webRequest.SendWebRequest();
         string[] temp=webRequest.downloadHandler.text.Split('^');
         int index=1;
-        var item=Instantiate(sentence,new Vector3(0,0,0),Quaternion.identity);
-        item.transform.SetParent(Content.transform);
         foreach(string i in temp)
         {
             if(i.Length!=0)
             {
-                Debug.Log(i);
+                sentence.GetComponent<Text>().text=index+"    "+i;
+                var item=Instantiate(sentence,new Vector3(0,0,0),Quaternion.identity);
+                item.transform.SetParent(Content[num].transform);
                 index++;
-            }
-
-            
+            }   
         }
     }
 }
