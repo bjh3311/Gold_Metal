70d147cde285dc8da825b370c1e22f8ba7066303
bjh3311 <bjh3311@naver.com>
2022-03-21 01:12:41 +0900

    네트워크 연결

--- Assets/Script/NetworkManager.cs
+++ Assets/Script/NetworkManager.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NetworkManager : MonoBehaviour
 {
+    private string NetworkUrl;
+    private Canvas canvas;
     private void Awake() 
     {
-        var obj=FindObjectOfType<NetworkManager>();
-        if(obj.Length==1)
-        {
-
-        }
-        
+        DontDestroyOnLoad(this.gameObject);
+        NetworkUrl="bjh3311.cafe24.com/Network.php";
+        canvas=this.gameObject.GetComponent<Canvas>();
     }
     void Update()
     {
-        
+        StartCoroutine("Check");
     }
-}
+    IEnumerator Check()
+    {
+        UnityWebRequest webRequest=UnityWebRequest.Get(NetworkUrl);
+        yield return webRequest.SendWebRequest();
+        if(webRequest.error!=null)//통신에 실패했다
+        {
+            canvas.gameObject.SetActive(true);
+            yield return new WaitForSecondsRealtime(2.5f);
+            Application.Quit();
+        }
+    }
+}   
