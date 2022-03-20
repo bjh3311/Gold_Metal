e67c3ddea5cf12f62ed321ed4af2a37f2ef6922c
bjh3311 <bjh3311@naver.com>
2022-03-20 23:48:15 +0900

    네트워크 연결

---
+++ Assets/Script/NetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    private void Awake() 
+    {
+        var obj=FindObjectOfType<NetworkManager>();
+        if(obj.Length==1)
+        {
+
+        }
+        
+    }
+    void Update()
+    {
+        
+    }
+}
